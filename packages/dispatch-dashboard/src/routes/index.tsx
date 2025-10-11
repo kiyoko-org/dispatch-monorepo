dbcc5f4a2d1b65c4e7786dc1cabea1dfdb8db84c
Steven <steven@Stevens-Mac-mini.local>
2025-10-11 08:53:20 +0800

    initial commit

---
+++ packages/dispatch-dashboard/src/routes/index.tsx
@@ -0,0 +1,106 @@
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Field, FieldError, FieldGroup } from '@/components/ui/field'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { createFileRoute } from '@tanstack/react-router'
+import { useForm } from '@tanstack/react-form'
+import * as z from "zod"
+
+export const Route = createFileRoute('/')({
+	component: App,
+})
+
+function App() {
+
+	const loginSchema = z.object({
+		email: z.string().email(),
+		password: z.string().min(6, "Password must be atlease 6 characters long")
+	})
+
+	const loginForm = useForm({
+		defaultValues: {
+			email: "",
+			password: ""
+		},
+		validators: {
+			onSubmit: loginSchema,
+		},
+		onSubmit: async ({ value }) => {
+			console.info("You submitted: ", value)
+		}
+	})
+
+	return (
+		<div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+			<div className="w-full max-w-sm">
+				<Card>
+					<CardHeader>
+						<CardTitle>Login to your account</CardTitle>
+					</CardHeader>
+					<CardContent>
+						<form onSubmit={(e) => {
+							e.preventDefault()
+							loginForm.handleSubmit()
+						}}>
+							<FieldGroup>
+								<loginForm.Field name='email' children={(field) => {
+									const isInvalid =
+										field.state.meta.isTouched && !field.state.meta.isValid
+
+									return (
+										<Field data-invalid={isInvalid}>
+											<Label htmlFor={field.name}>Email</Label>
+											<Input
+												id={field.name}
+												name={field.name}
+												value={field.state.value}
+												onBlur={field.handleBlur}
+												onChange={(e) => field.handleChange(e.target.value)}
+												aria-invalid={isInvalid}
+												placeholder="m@example.com"
+												autoComplete="off"
+												required
+											/>
+											{isInvalid && (
+												<FieldError errors={field.state.meta.errors} />
+											)}
+										</Field>
+									)
+								}
+								} />
+								<loginForm.Field name='password' children={(field) => {
+									const isInvalid =
+										field.state.meta.isTouched && !field.state.meta.isValid
+
+									return (
+										<Field data-invalid={isInvalid}>
+											<Label htmlFor={field.name}>Password</Label>
+											<Input
+												id={field.name}
+												name={field.name}
+												value={field.state.value}
+												onBlur={field.handleBlur}
+												onChange={(e) => field.handleChange(e.target.value)}
+												aria-invalid={isInvalid}
+												placeholder="Password"
+												type='password'
+												autoComplete="off"
+												required></Input>
+											{isInvalid && (
+												<FieldError errors={field.state.meta.errors} />
+											)}
+										</Field>
+									)
+								}} />
+								<Field>
+									<Button type="submit">Login</Button>
+								</Field>
+							</FieldGroup>
+						</form>
+					</CardContent>
+				</Card>
+			</div>
+		</div>
+	)
+}
---
+++ packages/dispatch-lib/index.ts
@@ -0,0 +1,42 @@
+import { createClient as createSupabaseClient, SupabaseClient, type SupportedStorage } from '@supabase/supabase-js';
+import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/SupabaseAuthClient';
+
+interface SupabaseClientOptions {
+	url: string,
+	anonymousKey: string,
+	storage?: SupportedStorage
+}
+
+interface DispatchClientOptions {
+	supabaseClientConfig: SupabaseClientOptions,
+}
+
+class DispatchAuthClient extends SupabaseAuthClient {
+
+}
+
+export class DispatchClient {
+
+	private supabase: SupabaseClient
+	auth: SupabaseAuthClient
+
+	constructor({ supabaseClientConfig }: DispatchClientOptions) {
+
+		this.supabase = createSupabaseClient(
+			supabaseClientConfig.url,
+			supabaseClientConfig.anonymousKey,
+			{
+				auth: {
+					storage: supabaseClientConfig.storage,
+					autoRefreshToken: true,
+					persistSession: true,
+					detectSessionInUrl: false,
+				}
+			}
+		)
+
+		this.auth = this.supabase.auth as DispatchAuthClient
+
+	}
+
+}
